75ae7cc485c8720d60000cd1647ba7f6b10b5fda
Dmytro Brodar <dm1brodar@gmail.com>
2025-11-07 16:12:20 +0000

    Initial project: structure, venv, requirements, template

---
+++ src/template_project2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  2 12:57:46 2025
+
+
+Student Name: ??????
+
+Student ID: ?????
+
+Cohort/Group/Course: ????
+
+"""
+
+# Please note that you need to implement the project using functions; avoid script-based programming as it attracts negative marks.
+
+# Do not use any library or any syntax that was not covered in the lecture or lab
+
+
