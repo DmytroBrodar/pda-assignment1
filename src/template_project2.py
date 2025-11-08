5b99a6a48ddb02c7dfb40751215917919d3aee45
Dmytro Brodar <dm1brodar@gmail.com>
2025-11-08 01:43:20 +0000

    Completed Task 1: product and shipping analysis

--- src/template_project2.py
+++ src/template_project2.py
@@ -1,19 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov  2 12:57:46 2025
-
-
-Student Name: ??????
-
-Student ID: ?????
-
-Cohort/Group/Course: ????
-
+Student Name: Dmytro 
+Student ID: R00274472
+Cohort/Group/Course: SDH3A 
 """
 
-# Please note that you need to implement the project using functions; avoid script-based programming as it attracts negative marks.
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Do not use any library or any syntax that was not covered in the lecture or lab
+# load data
+def load_data():
+    # get current script directory
+    folder = os.path.dirname(__file__)
+    # create full file path
+    data_path = os.path.join(folder,"..", "data", "shopping.csv")
+    # convert to absolute path
+    data_path = os.path.abspath(data_path)
+
+    # read csv file
+    df = pd.read_csv(data_path)
+
+    # clean column names, remove extra spaces
+    df.columns = df.columns.str.strip()
+
+    # # show column names
+    # print("\n--- Columns in shopping.csv ---")
+    # print(df.columns)
+
+    return df
+
+# Task 1: Analyze relationship between products and shipping types 
+def product_shipping(df):
+    # remove empty rows
+    df = df.dropna(how='all')
+
+    # remove spaces from text columns
+    for col in df.select_dtypes(include='object'):
+        df[col] = df[col].astype(str).str.strip()
+
+    # select important columns
+    product_col = "Item Purchased"
+    ship_col = "Shipping Type"
+
+    # remove rows with missing values
+    df = df.dropna(subset=[product_col, ship_col])
+
+    # count shipping for each product
+    counts = df.groupby([product_col, ship_col]).size().reset_index(name='Count')
+
+    # calculate persentages
+    counts['Percent'] = (counts['Count'] / counts.groupby(product_col)['Count'].transform('sum') * 100).round(2)
+
+    # sort and print
+    counts = counts.sort_values([product_col, 'Percent'], ascending=[True, False])
+
+    # print each product results
+    for product in counts[product_col].unique():
+        print(f"\nProduct: {product}")
+        sub = counts[counts[product_col] == product]
+        for _, row in sub.iterrows():
+            print(f" {row[ship_col]} - {row['Percent']}% (count: {row['Count']})")
 
 
+# main function
+def main():
+   product_shipping(load_data())
+
+
+main()
