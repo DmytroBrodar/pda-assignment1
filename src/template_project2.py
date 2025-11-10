f05501cbc2bbe19deff61f2719700bb714755c7c
Dmytro Brodar <dm1brodar@gmail.com>
2025-11-10 01:42:40 +0000

    Added Task 2: customer segmentation by gender and visualization

--- src/template_project2.py
+++ src/template_project2.py
@@ -52,8 +52,8 @@ def product_shipping(df):
     # count shipping for each product
     counts = df.groupby([product_col, ship_col]).size().reset_index(name='Count')
 
-    # calculate persentages
-    counts['Percent'] = (counts['Count'] / counts.groupby(product_col)['Count'].transform('sum') * 100).round(2)
+    # calculate persentages (divide each shipping count by the total number of orders of that product row by row)
+    counts['Percent'] = (counts['Count'] / counts.groupby(product_col)['Count'].transform('sum') * 100).round()
 
     # sort and print
     counts = counts.sort_values([product_col, 'Percent'], ascending=[True, False])
@@ -63,12 +63,54 @@ def product_shipping(df):
         print(f"\nProduct: {product}")
         sub = counts[counts[product_col] == product]
         for _, row in sub.iterrows():
-            print(f" {row[ship_col]} - {row['Percent']}% (count: {row['Count']})")
+            print(f" {row[ship_col]} - {row['Percent']}% (amount: {row['Count']})")
 
+# -----------------------------------------------------------------------------------
+# Task2: customer segments separated by gender
+def customer_segments(df):
+    # removing extra space
+    for col in df.select_dtypes(include='object'):
+        df[col] = df[col].astype(str).str.strip()
+
+    # convert columns to numeriic values
+    df["Purchase Amount (USD)"] = pd.to_numeric(df["Purchase Amount (USD)"], errors='coerce')
+    df["Previous Purchases"] = pd.to_numeric(df["Previous Purchases"], errors='coerce')
+
+    # drop rows with missing or wrong values
+    df = df.dropna(subset=["Purchase Amount (USD)", "Previous Purchases", "Gender"])
+
+    # create new column
+    df["Total Purchased USD"] = df["Purchase Amount (USD)"] * df["Previous Purchases"]
+
+    # definr segments
+    bins = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000]
+    labels = ["Segment 1", "Segment 2", "Segment 3", "Segment 4", "Segment 5", "Segment 6", 
+              "Segment 7", "Segment 8", "Segment 9", "Segment 10", "Segment 11", "Segment 12" ]
+    
+    df["Segment"] = pd.cut(df["Total Purchased USD"], bins=bins, labels=labels, include_lowest=True)
+
+    # group by gender and segment
+    group_counts = df.groupby(["Segment", "Gender"]).size()
+
+    print("----- Population of each gender ------")
+    print(group_counts)
+
+    # visualization
+    vis = group_counts.plot(kind='bar', figsize=(10,8), color=['green', 'blue'])
+    vis.set_title("Customer Segments by gender")
+    vis.set_xlabel("Segments")
+    vis.set_ylabel("Number of customers")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
 
 # main function
 def main():
-   product_shipping(load_data())
+    # Task 1
+    # product_shipping(load_data())
+    # Task 2
+    customer_segments(load_data())
 
 
-main()
+if __name__ == "__main__":
+    main()
