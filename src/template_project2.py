aee202a021c88dab2d987590c71ff76c739a9cef
Dmytro Brodar <dm1brodar@gmail.com>
2025-11-11 21:52:46 +0000

    Completed Task 4: date analysis with pie, line, and histogram visualizations, made some fix with Task 2

--- src/template_project2.py
+++ src/template_project2.py
@@ -82,47 +82,57 @@ def customer_segments(df):
     # create new column
     df["Total Purchased USD"] = df["Purchase Amount (USD)"] * df["Previous Purchases"]
 
-    # definr segments
-    bins = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000]
-    labels = ["Segment 1", "Segment 2", "Segment 3", "Segment 4", "Segment 5", "Segment 6", 
-              "Segment 7", "Segment 8", "Segment 9", "Segment 10", "Segment 11", "Segment 12" ]
-    
-    df["Segment"] = pd.cut(df["Total Purchased USD"], bins=bins, labels=labels, include_lowest=True)
+    # definr 12 segments (0–500, 500–1000, ...)
+    valid = df["Total Purchased USD"] >= 0
+    df = df[valid].copy()
+
+    idx = ((df["Total Purchased USD"] - 1) // 500).astype("int64")
+    idx = idx.clip(lower=0, upper=11)
+
+    # make segment names
+    label_map = {i: f"Segment{i+1}" for i in range(12)}
+    df["Segment"] = idx.map(label_map)
 
     # group by gender and segment
-    group_counts = df.groupby(["Segment", "Gender"]).size().unstack()
+    group_counts = df.groupby(["Segment", "Gender"]).size().unstack(fill_value=0)
 
-    print("----- Population of each gender ------")
-    print(group_counts)
+    # dictionary to show numeric ranges
+    segment_labels = {
+        "Segment1": "0-500",
+        "Segment2": "500-1000",
+        "Segment3": "1000-1500",
+        "Segment4": "1500-2000",
+        "Segment5": "2000-2500",
+        "Segment6": "2500-3000",
+        "Segment7": "3000-3500",
+        "Segment8": "3500-4000",
+        "Segment9": "4000-4500",
+        "Segment10": "4500-5000",
+        "Segment11": "5000-5500",
+        "Segment12": "5500-6000"
+    }
 
+    # sort index by numeric part
+    group_counts = group_counts.reindex(sorted(group_counts.index, key=lambda x: int(x.replace("Segment", ""))))
 
-    # Replace segment names with numeric ranges 
-    # segment_labels = {
-    #     "Segment 1": "0-500",
-    #     "Segment 2": "500-1000",
-    #     "Segment 3": "1000-1500",
-    #     "Segment 4": "1500-2000",
-    #     "Segment 5": "2000-2500",
-    #     "Segment 6": "2500-3000",
-    #     "Segment 7": "3000-3500",
-    #     "Segment 8": "3500-4000",
-    #     "Segment 9": "4000-4500",
-    #     "Segment 10": "4500-5000",
-    #     "Segment 11": "5000-5500",
-    #     "Segment 12": "5500-6000"
-    # }
+    # print with both segment name and range
+    print("\n--- Segment names with ranges ---")
+    for seg, row in group_counts.iterrows():
+        range_label = segment_labels.get(seg, "Unknown")
+        female = row.get("Female", 0)
+        male = row.get("Male", 0)
+        print(f"{seg} ({range_label}): Female={female}, Male={male}")
 
-    # # update index labels
-    # group_counts.index = group_counts.index.map(segment_labels)
-    # print(group_counts)
-
-
+    # update index labels
+    group_counts.index = group_counts.index.map(segment_labels)
+    
     # visualization
-    vis = group_counts.plot(kind='bar', figsize=(10,8), color=['green', 'blue']) # green = female, blue = male
+    vis = group_counts.plot(kind='bar', figsize=(8,6)) 
     vis.set_title("Customer Segments by gender")
     vis.set_xlabel("Total Purchased USD Range")
     vis.set_ylabel("Number of customers")
     vis.legend(title="Gender")
+    plt.tight_layout()
     plt.xticks(rotation=45)
     plt.show()
 
@@ -186,6 +196,111 @@ def product_analysis(df):
     plt.show() 
 
 
+# -----------------------------------------------------------------------------------
+# Task 4: DAte Analysis
+def date_analysis(df):
+    # clean date columns
+    df["Dates"] = pd.to_datetime(df["Dates"],errors='coerce')
+    df = df.dropna(subset=["Dates"])
+    
+    # extract date
+    df["Month"] = df["Dates"].dt.month
+    df["Year"] = df["Dates"].dt.year
+
+    # define seasons (4 seasons, start Mar 21 and lasts three months) 
+    def get_season(date):
+        month = date.month
+        day = date.day
+        if(month == 3 and day>=21) or month in [4,5] or (month == 6 and day <= 20):
+            return "Spring" 
+        elif(month == 6 and day>=21) or month in [7,8] or (month == 9 and day <= 20):
+            return "Summer"
+        elif (month == 9 and day>=21) or month in [10,11] or (month == 12 and day <=20):
+            return "Autumn"
+        else:
+            return "Winter"
+        
+    df["Season"] = df["Dates"].apply(get_season)
+
+    # ------ popularity of season ------
+    season_counts = df["Season"].value_counts()
+    print("Sales in season")
+    print(season_counts)
+    
+    # visualizations
+    plt.figure(figsize=(6,6))
+    season_counts.plot(kind="pie", autopct='%1.1f%%', colors=['red','green','blue','yellow'])
+    plt.title("Seasonal Popularity (Sales Share)")
+    plt.ylabel("")  
+    plt.show()
+
+    # ------ popularity of months ------
+    month_counts = df["Month"].value_counts().sort_index()
+
+    # print first 3 months
+    top_months = month_counts.sort_values(ascending=False).head(3)
+    
+    print("\nMost 3 popular months:")
+
+    month_names = {
+    1: "January", 2: "February", 3: "March", 4: "April",
+    5: "May", 6: "June", 7: "July", 8: "August",
+    9: "September", 10: "October", 11: "November", 12: "December"
+    }
+
+    for month, count in top_months.items():
+        name = month_names[month]
+        print(f"{name}: {count} sales")
+
+    month_counts.index = month_counts.index.map(month_names)
+    
+    # visualization
+    plt.figure(figsize=(10,6))
+    plt.plot(
+        month_counts.index,          # x-axis (month names)
+        month_counts.values,         # y-axis (sales counts)
+        marker='o'                            
+    )
+    plt.title("Monthly Popularity (Number of Sales)")
+    plt.xlabel("Month")
+    plt.ylabel("Sales")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    # ----- yearly product sales ------
+    def product_yearly_sale(prod_name):
+        sub = df[df["Item Purchased"].str.lower() == prod_name.lower()]
+        if sub.empty:
+            print("No sales information for this product.")
+            return
+       
+        print(f" \nYearly sales for {prod_name}")
+        print(sub["Year"].value_counts().sort_index())
+
+        plt.figure(figsize=(10,6))
+        sns.histplot(
+            data=sub,
+            x="Year",             # which column to plot
+            bins=len(sub["Year"].unique()),  # one bin per year
+            kde=True
+        )
+        plt.title(f"Yearly Sales Distribution for {prod_name}")
+        plt.xlabel("Year")
+        plt.ylabel("Number of Sales")
+        plt.tight_layout()
+        plt.show()
+
+    # ask user to type product name
+    # prod_name = input("\nEnter the product name to see its yearly sales: ")
+
+    # call the function
+    # product_yearly_sale(prod_name)
+
+    # example output
+    sample = df["Item Purchased"].iloc[0]
+    print(f"Example for {sample}")
+    product_yearly_sale(sample)
 
 
 
@@ -197,7 +312,9 @@ def main():
     # Task 2
     # customer_segments(df)
     # Task 3
-    product_analysis(df)
+    # product_analysis(df)
+    # Task 4
+    date_analysis(df)
 
 
 if __name__ == "__main__":
